f529e4f829ecc35faba683e82833348402912cd1
googlr <googlr@users.noreply.github.com>
2017-11-01 17:58:13 -0400

    Update bigram.py

--- lab5_spark/bigram.py
+++ lab5_spark/bigram.py
@@ -23,12 +23,6 @@ if __name__ == "__main__":
             bigrams.append( ((s[i],s[i+1]),1))
 
         return sc.parallelize(bigrams)
-    #bigrams = []
-    #for sentence in sentences:
-     #   s = sentence.split(" ")
-      #  for i in range(0, len(s)-1):
-       #     bigrams.append( ((s[i],s[i+1]), 1) )
-
 
     top100bigram = sentences.map(lambda s: get_bigram(s)).reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[1], False).take(100)
     top100bigram.saveAsTextFile("top100bigram.out")
